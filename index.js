4f002129ac5bb0eb8c4fbef8cc5dd4d64fb043ba
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-06-05 09:00:16 +0100

    assetmgt

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ app.get(`/assetmgt`, (_req, res) => {
     res.render(`assetmgt`);
 });
 
+app.get(`/assetmgt/assets`, (_req, res) => {
+    res.render(`assets`);
+});
+
+app.get(`/assetmgt/locations`, (_req, res) => {
+    res.render(`locations`);
+});
+
+app.get(`/assetmgt/asseteditor`, (_req, res) => {
+    res.render(`asseteditor`);
+});
+
+app.get(`/assetmgt/locationeditor`, (_req, res) => {
+    res.render(`locationeditor`);
+});
+
 app.get(`/critsys`, (_req, res) => {
     res.render(`critsys`);
 });
