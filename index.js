0b1a3f296689860c68f3519ba93f5c1c7c622650
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2021-02-10 16:52:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function getassets() {
         let connection = await pool.getConnection();
         let rows = await connection.query(`SELECT * FROM assets`);
         connection.end();
-        return rows.slice(-1, rows.length-1);
+        return rows.slice(0, -1);
     } catch (error) {
         fs.appendFile(`./logs/error.log`, `${error}\n`, (error) => {
             if (error) {
