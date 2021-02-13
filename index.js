7250dd7c2b96ad384fe55d412da02c86e04dd554
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2021-02-13 17:04:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -260,7 +260,7 @@ app.post(`/assetmgt/asseteditor/*`, async function (req, res) {
             let ID = req.originalUrl.split(`/`)[3];
             let result = await getasset(ID);
             if (typeof result != `undefined`) {
-                let json = `{ "Title": req.body.Title, "Contents": req.body.Contents, "Location": req.body.Location, "Notes": req.body.Notes }`;
+                let json = `{ "Title": "${req.body.Title}", "Contents": "${req.body.Contents}", "Location": "${req.body.Location}", "Notes": "${req.body.Notes}" }`;
                 let updatedrecord = JSON.parse(json);
                 console.log(updatedrecord.Title);
                 for (i in updatedrecord) {
