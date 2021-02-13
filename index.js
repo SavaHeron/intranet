57f2a003f34ad8585d2cdac668dce6e6e14d53ae
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2021-02-13 17:49:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ app.post(`/assetmgt/asseteditor/*`, async function (req, res) {
                 let updatedrecord = { Title: req.body.Title, Contents: req.body.Contents, Location: req.body.Location, Notes: req.body.Notes };
                 Object.keys(updatedrecord).forEach(key => {
                     if (!updatedrecord[key]) {
-                        console.log(result[ID]);
+                        console.log(key);
                         console.log(result["ID"]);
                     };
                 });
