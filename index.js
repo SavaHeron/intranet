04bfdae6b37af9633afc62174ceba681f15bb768
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2021-02-13 16:23:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.post(`/assetmgt/asseteditor/*`, async function (req, res) {
                         updatedrecord[key] = result[key];
                     };
                 };
-                await updateasset(ID, updatedrecord.Title, updatedrecord.Contents, updatedrecord.Location, updatedrecord.Notes);
+                await updateasset(ID, JSON.stringify(updatedrecord.Title), JSON.stringify(updatedrecord.Contents), JSON.stringify(updatedrecord.Location), JSON.stringify(updatedrecord.Notes));
                 return res.redirect(`/assetmgt/asseteditor/${ID}`);
             } else {
                 return res.redirect(`/error/404`);
